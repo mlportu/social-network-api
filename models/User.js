9464ed6024f0cdd0225391108ec97cdd27495d97
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-13 11:50:49 -0600

    user model structure

---
+++ models/User.js
@@ -0,0 +1,35 @@
+const { Schema, model } = require('mongoose');
+
+const userSchema = new Schema(
+    {
+        username: {
+            type: String,
+            require: true,
+            unique: true,
+            trim: true,
+        },
+        email: {
+            type:String,
+            required: true, 
+            unique: true, 
+            match: [/.+@.+\..+/, 'Must match an email address!'],
+        },
+        thoughts:[
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought',
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref:'User',
+            },
+        ],
+    },
+    {
+        toJSON:{
+            virtuals: true,
+        },
+    }
+)
