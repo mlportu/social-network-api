3b70b80cc7165d1ac3c00daea978ac975d56629c
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-13 12:41:12 -0600

    api routes connected

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
+router.use('/api', apiRoutes);
+
 router.use((req, res) => {
     return res.send('404 Error')
 })
