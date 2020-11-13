d533164b1cda103baaedac0de5a6d84bcf020bb8
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-13 12:32:41 -0600

    user routes

---
+++ routes/api/user-routes.js
@@ -0,0 +1,18 @@
+const router = require('express').Router();
+
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser
+} = require('../../controllers/user-controllers');
+const { update } = require('../../models/User');
+
+//GET and POST routes @ /api/users
+router.route('/').get(getUsers).post(createUser);
+
+// GET one, PUT, and Delete routes @ /api/users/:id
+router.route('/:id').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+module.exports = router
