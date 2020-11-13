cbbe85464a54cd52f231db0c15075abe98cf0d56
Michael Portu <michaelportu@Michaels-MacBook-Pro.local>
2020-11-13 12:27:30 -0600

    full user CRUD

--- controllers/user-controllers.js
+++ controllers/user-controllers.js
@@ -15,6 +15,7 @@ const userController = {
     getSingleUser(req, res) {
         User.findOne({ _id: req.params.userId })
           .then((dbUserData) => {
+              //if no user with this id
             if (!dbUserData) {
               return res.status(404).json({ message: 'No user with this id!' });
             }
@@ -36,6 +37,36 @@ const userController = {
             res.status(500).json(err);
           });
       },
+      //update user by id
+      updateUser(req, res) {
+          User.findByIdAndUpdate({ _id: req.params.id}, body, {new:true})
+          .then(dbUserData => {
+              if(!dbUserData) {
+                  res.json(404).json({message: 'No user found with this id'});
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch((err) => {
+              console.log(err);
+              res.status(500).json(err);          
+            });
+      },
+      // detele user 
+      deleteUser(req, res) {
+          User.findByIdAndDelete({_id: params.id})
+          .then(dbUserData => {
+              if(!dbUserData){
+                  res.ststus(404).json({message:'No user found with this id'});
+                  return;
+              }
+              res.json(dbUserData);
+          })
+          .catch((err) => {
+            console.log(err);
+            res.status(500).json(err);          
+          });
+      }
 };
 
 module.exports = userController
